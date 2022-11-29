e3cb141c66b2caddf31a28c45321feebb605de47
RominaVidalVargas <roomii.2010.vidal@gmail.com>
2022-11-29 15:07:33 -0300

    trivia 99%

--- src/main.js
+++ src/main.js
@@ -1,3 +1,19 @@
-/*
-  Aquí va tu JavaScript
-*/
+let name= prompt("Como te llamas?").toUpperCase();
+function mostrarTrivia(){
+document.getElementById("trivia").style.display="block";
+document.getElementById("borrar").style.display="none";
+mostrarTrivia()
+}
+let counter = 0;
+function reinicio(){
+document.getElementById("trivia").reset();
+}
+function correcta() {
+ 
+    //alert ("Respuesta Correcta")
+    counter++;
+  
+};
+function corregir(){
+  alert ( name +  " Tu Puntaje Final es: " +  counter)
+}
